2e85bb607e69dd2a206301592891302219e91f3f
Yang <yceyoung@gmail.com>
2017-12-21 20:19:15 +0800

    create a admin's routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #resources :restaurants
+
+  # Regular sser's routes
   root "restaurants#index"
+
+  # Admin's routes
+  namespace :admin do
+    root "restaurants#index"
+  end
+
 end
