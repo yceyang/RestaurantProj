cd2ae2f7621e33c2cf8c6e3ee0c23cba3619bff0
Yang <yceyoung@gmail.com>
2017-12-24 22:37:24 +0800

    set up admin authentication

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,2 +1,6 @@
 class Admin::RestaurantsController < ApplicationController
+  # user login 
+  before_action :authenticate_user!
+  # self define admin action
+  before_action :authenticate_admin
 end
