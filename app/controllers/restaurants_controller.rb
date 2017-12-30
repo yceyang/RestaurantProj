94b1554f044714eb4aeed3da57f7cead6ce7744d
Yang <yceyoung@gmail.com>
2017-12-30 12:26:34 +0800

    change user authentication flow

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,9 @@
 class RestaurantsController < ApplicationController
   # user login 
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   # index
   def index
-    
+    @restaurants = Restaurant.page(params[:page]).per(20)
   end
 end
