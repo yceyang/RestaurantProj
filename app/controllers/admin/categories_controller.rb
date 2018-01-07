1ec3d87756cdc2ccc6a1252739fc925f887d1f53
Yang <yceyoung@gmail.com>
2018-01-07 17:21:17 +0800

    user can update category with the same form on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,19 +5,34 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    @category = Category.new
+
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 
   def create
     @category = Category.create(category_params)
 
     if @category.save
-      flash[:notice] = "category was successfully created"
       redirect_to admin_categories_path
+      flash[:notice] = "category was successfully created"
     else
-      flash[:alert] = "category was failed to created"
-
       @categories = Category.all # render to index, but not has categories instance, so must be set.
+      render :index
+      flash[:alert] = "category was failed to created"
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:notice] = "category was successfully updated"
+    else
+      @categories = Category.all
       render :index
     end
   end
