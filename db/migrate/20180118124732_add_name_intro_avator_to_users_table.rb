366af8165a12a0ebf5b1328c5150db31fe7313e6
Yang <yceyoung@gmail.com>
2018-01-18 20:50:51 +0800

    add name/intro/avator column to users table

---
+++ db/migrate/20180118124732_add_name_intro_avator_to_users_table.rb
@@ -0,0 +1,7 @@
+class AddNameIntroAvatorToUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :intro, :text
+    add_column :users, :avator, :string
+  end
+end
