244b4ac522bfd47d2f93e8e3530457878043c856
Yang <yceyoung@gmail.com>
2018-01-30 18:49:51 +0800

    add like / unlike partial to restaurant index and show views. add is_liked? method to change like / unlike botton

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -14,4 +14,8 @@ class Restaurant < ApplicationRecord
   def is_favorited?(user)
     self.favorited_users.include?(user)
   end
+
+  def is_liked?(user)
+    self.liked_users.include?(user)
+  end
 end
